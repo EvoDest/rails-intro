34ce0cfe92dd1cd19a11db008a57ab89b3861667
saasbook <saasbook@saasbook.(none)>
2014-05-24 11:19:42 -0300

    HW2 Part 1 initial code change

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,12 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort = params[:sort_by]
+    if !@sort.nil? 
+	@movies = Movie.order(@sort).all
+    else
+        @movies = Movie.all
+    end
   end
 
   def new
